0032e205567e778176e1dab004df5154a666e45f
David Cournapeau <cournape@gmail.com>
2025-09-20 15:16:09 +0900

    REF: extract arnoldi_decomp function.

--- src/arnoldi/decomposition.py
+++ src/arnoldi/decomposition.py
@@ -2,6 +2,9 @@ import numpy as np
 import numpy.linalg as nlin
 
 
+norm = nlin.norm
+
+
 class Arnoldi:
     """ Create an arnoldi solver for operators of dimension n, with a Krylov
     basis of k dimensions.
@@ -29,24 +32,9 @@ class Arnoldi:
 
         self.q[:, 0] = init_vector
 
-    def iterate(self, a, tol=None):
-        tol = tol or self._atol
-        for j in range(self.k):
-            v = self.q[:, j+1]
-            v[:] = a @ self.q[:, j]
-
-            # Modified Gram-Schmidt (orthonormalization)
-            for i in range(j + 1):
-                self.h[i, j] = np.vdot(self.q[:, i], v)
-                v -= self.h[i, j] * self.q[:, i]
-
-            self.h[j + 1, j] = np.linalg.norm(v)
-
-            if self.h[j + 1, j] < self._atol:
-                raise ValueError("Lucky break not supported yet")
-            v /= self.h[j + 1, j]
-
-        return self.k
+    def iterate(self, a):
+        _, _, m = arnoldi_decomp(a, self.q, self.h, self.k, self._atol)
+        return m
 
     def _extract_arnold_decomp(self, size=None):
         """ Return Q_k/H_k such as Q_k^H A Q_k = H_k.
@@ -91,3 +79,46 @@ def _largest_eigvals(h, n_ev):
     eigvals, eigvecs = nlin.eig(h)
     ind = np.argsort(np.abs(eigvals))[:-n_ev-1:-1]
     return eigvals[ind], eigvecs[:, ind]
+
+
+def arnoldi_decomp(A, V, H, max_dim, invariant_tol):
+    """Run the arnoldi decomposition for square matrix a of dimension n.
+
+    Parameters
+    ----------
+    A : ndarray of shap (n, n)
+        square matrix to be decomposed
+    V : ndarray of shape (n, m+1)
+        the krylov basisexpected built by the Arnoldi decomposition
+    H : ndarray of shape (m+1, m)
+        H[:m, :m] is the Hessenberg matrix built by the Arnoldi decomposition
+    invariant_tol: float
+        threshold that contols when the decomposition will detect the Arnoldi
+        decomposition detected an invariant, i.e. when a new vector in Arnoldi
+        decomposition has a norm below that threshold.
+    """
+    n = A.shape[0]
+    m = V.shape[1] - 1
+
+    assert A.shape[1] == n, "A is expected to be square matrix"
+    assert V.shape == (n, m+1), "V must has same number of rows as V"
+    assert H.shape == (m+1, m), f"H must be {m+1, m}, is {H.shape}"
+
+    assert max_dim <= m, "max_dim > m"
+
+    for j in range(max_dim):
+        v = V[:, j+1]
+        v[:] = A @ V[:, j]
+
+        # Modified Gram-Schmidt (orthonormalization)
+        for i in range(j + 1):
+            H[i, j] = np.vdot(V[:, i], v)
+            v -= H[i, j] * V[:, i]
+
+        H[j + 1, j] = norm(v)
+
+        if H[j + 1, j] < invariant_tol:
+            raise ValueError("Lucky break not supported yet")
+        v /= H[j + 1, j]
+
+    return V, H, m
