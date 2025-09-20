967103d2e923eb4934ec0a325347fe94381817bb
David Cournapeau <cournape@gmail.com>
2025-09-20 15:33:00 +0900

    REF+STY: use upper case for matrices and rename  for clarity.
    k->m rename is to avoid confusion between k the number of eigen values
    we look for and m the dimension of the Krylov space.
    Q->V is more cosmetic, and to be more consistent with the literature.

--- src/arnoldi/decomposition.py
+++ src/arnoldi/decomposition.py
@@ -7,18 +7,18 @@ norm = nlin.norm
 
 class Arnoldi:
     """ Create an arnoldi solver for operators of dimension n, with a Krylov
-    basis of k dimensions.
+    basis of up to m dimensions.
     """
-    def __init__(self, n, k, dtype=np.complex128):
+    def __init__(self, n, m, dtype=np.complex128):
         self.n = n
-        self.k = k
+        self.m = m
 
-        self.q = np.zeros((n, k+1), dtype=dtype)
-        self.h = np.zeros((k+1, k), dtype=dtype)
+        self.V = np.zeros((n, m+1), dtype=dtype)
+        self.H = np.zeros((m+1, m), dtype=dtype)
 
     @property
     def _dtype(self):
-        return self.h.dtype
+        return self.H.dtype
 
     @property
     def _atol(self):
@@ -30,58 +30,58 @@ class Arnoldi:
             init_vector = np.random.randn(self.n).astype(self._dtype)
             init_vector /= np.linalg.norm(init_vector)
 
-        self.q[:, 0] = init_vector
+        self.V[:, 0] = init_vector
 
-    def iterate(self, a):
-        _, _, m = arnoldi_decomp(a, self.q, self.h, self.k, self._atol)
+    def iterate(self, A):
+        _, _, m = arnoldi_decomp(A, self.V, self.H, self._atol, self.m)
         return m
 
-    def _extract_arnold_decomp(self, size=None):
-        """ Return Q_k/H_k such as Q_k^H A Q_k = H_k.
+    def _extract_arnold_decomp(self, m=None):
+        """ Return V_m/H_m such as V_m^H A V_m = H_m.
         """
-        q, h = self.q, self.h
-        size = size or self.k
+        Q, H = self.V, self.H
+        m = m or self.m
 
-        q_k = q[:, :size]
-        h_k = h[:size, :size]
+        q_m = Q[:, :m]
+        h_m = H[:m, :m]
 
-        return q_k, h_k
+        return q_m, h_m
 
-    def _extract_ritz_decomp_and_raw_base(self, n_ritz, k=None):
-        k = k or self.k
-        q_k, h_k = self._extract_arnold_decomp(k)
+    def _extract_ritz_decomp_and_raw_base(self, n_ritz, m=None):
+        m = m or self.m
+        Q_m, H_m = self._extract_arnold_decomp(m)
 
-        ritz_values, vectors = _largest_eigvals(h_k, n_ritz)
-        ritz_vectors = q_k @ vectors
+        ritz_values, vectors = _largest_eigvals(H_m, n_ritz)
+        ritz_vectors = Q_m @ vectors
         return ritz_values, ritz_vectors, vectors
 
-    def _extract_ritz_decomp(self, n_ritz, k=None):
+    def _extract_ritz_decomp(self, n_ritz, m=None):
         """ Extract n_ritz ritz values / vectors."""
-        ritz_values, ritz_vectors, vectors = self._extract_ritz_decomp_and_raw_base(n_ritz, k)
+        ritz_values, ritz_vectors, vectors = self._extract_ritz_decomp_and_raw_base(n_ritz, m)
         return ritz_values, ritz_vectors
 
-    def _approximate_residuals(self, n_ritz, k=None):
+    def _approximate_residuals(self, n_ritz, m):
         # For a given eigen value/vector pair lambda_i / u_i, we have:
         #
         # ||A u_i - lambda_i u_i|| = h[k, k-1] * |<e_k, v_k>| where u_k = q * v_k
         #
         # See e.g. proposition 6.8 of
         # https://www-users.cse.umn.edu/~saad/eig_book_2ndEd.pdf
-        _, _, v_k = self._extract_ritz_decomp_and_raw_base(n_ritz, k)
-        return np.abs(self.h[k, k-1] * v_k[-1])
+        _, _, V_m = self._extract_ritz_decomp_and_raw_base(n_ritz, m)
+        return np.abs(self.H[m, m-1] * V_m[-1])
 
-    def _residuals(self, a, n_ritz, k=None):
-        ritz_values, ritz_vectors = self._extract_ritz_decomp(n_ritz, k)
-        return nlin.norm(a @ ritz_vectors - ritz_values * ritz_vectors, axis=0)
+    def _residuals(self, A, n_ritz, m=None):
+        ritz_values, ritz_vectors = self._extract_ritz_decomp(n_ritz, m)
+        return nlin.norm(A @ ritz_vectors - ritz_values * ritz_vectors, axis=0)
 
 
-def _largest_eigvals(h, n_ev):
-    eigvals, eigvecs = nlin.eig(h)
+def _largest_eigvals(H, n_ev):
+    eigvals, eigvecs = nlin.eig(H)
     ind = np.argsort(np.abs(eigvals))[:-n_ev-1:-1]
     return eigvals[ind], eigvecs[:, ind]
 
 
-def arnoldi_decomp(A, V, H, max_dim, invariant_tol):
+def arnoldi_decomp(A, V, H, invariant_tol, max_dim=None):
     """Run the arnoldi decomposition for square matrix a of dimension n.
 
     Parameters
@@ -96,6 +96,8 @@ def arnoldi_decomp(A, V, H, max_dim, invariant_tol):
         threshold that contols when the decomposition will detect the Arnoldi
         decomposition detected an invariant, i.e. when a new vector in Arnoldi
         decomposition has a norm below that threshold.
+    max_dim : int
+        Max dimension of the Krylov space. By default, guessed from V.shape
     """
     n = A.shape[0]
     m = V.shape[1] - 1
@@ -104,7 +106,8 @@ def arnoldi_decomp(A, V, H, max_dim, invariant_tol):
     assert V.shape == (n, m+1), "V must has same number of rows as V"
     assert H.shape == (m+1, m), f"H must be {m+1, m}, is {H.shape}"
 
-    assert max_dim <= m, "max_dim > m"
+    max_dim = max_dim or m
+    assert max_dim <= m, "max_dim > m violated"
 
     for j in range(max_dim):
         v = V[:, j+1]
--- tests/test_decomposition.py
+++ tests/test_decomposition.py
@@ -21,40 +21,40 @@ def basis_vector(n, k, dtype=np.int64):
 
 class TestArnoldiExpansion:
     def test_invariant_simple(self):
-        # Test the invariant A * Q ~ Q * H, with H Hessenberg matrix and Q
+        # Test the invariant A * V ~ V * H, with H Hessenberg matrix and V
         # orthonormal
 
         ## Given
         n = 10
-        k = 6
+        m = 6
         dtype = np.complex128
-        e_k = basis_vector(k, k, dtype)
+        e_k = basis_vector(m, m, dtype)
 
-        a = sp.random(n, n, density=5 / n, dtype=dtype)
-        a += sp.diags_array(np.ones(n))
+        A = sp.random(n, n, density=5 / n, dtype=dtype)
+        A += sp.diags_array(np.ones(n))
 
         ## When
-        arnoldi = Arnoldi(n, k)
+        arnoldi = Arnoldi(n, m)
         arnoldi.initialize()
-        n_iter = arnoldi.iterate(a)
+        n_iter = arnoldi.iterate(A)
 
-        q, h = arnoldi.q, arnoldi.h
-        q_k, h_k = arnoldi._extract_arnold_decomp(n_iter)
+        V, H = arnoldi.V, arnoldi.H
+        V_m, H_m = arnoldi._extract_arnold_decomp(n_iter)
 
         ## Then
-        # the arnoldi basis Q is orthonormal
+        # the arnoldi basis V is orthonormal
         np.testing.assert_allclose(
-            q.conj().T @ q, np.eye(n_iter + 1), rtol=RTOL, atol=ATOL
+            V.conj().T @ V, np.eye(n_iter + 1), rtol=RTOL, atol=ATOL
         )
         # the arnoldi decomposition invariants are respected
         np.testing.assert_allclose(
-            a @ q_k,
-            q_k @ h_k + h[-1, -1] * np.outer(q[:, -1], e_k),
+            A @ V_m,
+            V_m @ H_m + H[-1, -1] * np.outer(V[:, -1], e_k),
             rtol=RTOL,
             atol=ATOL,
         )
 
-        np.testing.assert_allclose(a @ q[:, :-1], q @ h, rtol=RTOL, atol=ATOL)
+        np.testing.assert_allclose(A @ V[:, :-1], V @ H, rtol=RTOL, atol=ATOL)
 
     @pytest.mark.flaky(reruns=3)
     def test_eigvals_simple(self):
@@ -65,20 +65,20 @@ class TestArnoldiExpansion:
         n = 20
         # We use large krylov space since basic arnoldi does not converge
         # quickly. Once we implement restarts, it should converge much faster
-        k = n - 1
+        m = n - 1
         n_ev = 3
 
-        a = sp.random(n, n, density=5 / n, dtype=np.complex128)
+        A = sp.random(n, n, density=5 / n, dtype=np.complex128)
         # We add ones on the diag to have a well conditioned matrix and eigen
         # values not too far from 1
-        a += sp.diags_array(np.ones(n))
+        A += sp.diags_array(np.ones(n))
 
-        r_eigvals, _ = _largest_eigvals(a.toarray(), n_ev)
+        r_eigvals, _ = _largest_eigvals(A.toarray(), n_ev)
 
         ## When
-        arnoldi = Arnoldi(n, k)
+        arnoldi = Arnoldi(n, m)
         arnoldi.initialize()
-        n_iter = arnoldi.iterate(a)
+        n_iter = arnoldi.iterate(A)
 
         ritz_values, _ = arnoldi._extract_ritz_decomp(n_ev, n_iter)
 
@@ -89,18 +89,18 @@ class TestArnoldiExpansion:
     def test_residuals_computation(self):
         ## Given
         n = 20
-        k = 6
+        m = 6
         n_ev = 3
 
-        a = sp.random(n, n, density=5 / n, dtype=np.complex128)
+        A = sp.random(n, n, density=5 / n, dtype=np.complex128)
         # We add ones on the diag to have a well conditioned matrix and eigen
         # values not too far from 1
-        a += sp.diags_array(np.ones(n))
+        A += sp.diags_array(np.ones(n))
 
         ## When
-        arnoldi = Arnoldi(n, k)
+        arnoldi = Arnoldi(n, m)
         arnoldi.initialize()
-        n_iter = arnoldi.iterate(a)
+        n_iter = arnoldi.iterate(A)
 
         ## Then
         # Ensure residuals and approximate residuals match
@@ -114,5 +114,5 @@ class TestArnoldiExpansion:
         # "true" residual, the right hand side the approximate one
         for i in range(1, n_iter):
             residuals = arnoldi._approximate_residuals(n_ev, i)
-            r_residuals = arnoldi._residuals(a, n_ev, i)
+            r_residuals = arnoldi._residuals(A, n_ev, i)
             np.testing.assert_allclose(r_residuals, residuals, rtol=RTOL, atol=ATOL)
