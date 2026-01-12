b988d8ea400a22e6a6860c946ae6c33fdea31e83
David Cournapeau <cournape@gmail.com>
2026-01-12 18:46:40 +0900

    FEAT: add function to reorder Schur (complex mode only).
    This will be useful to test explicitly restarted Arnoldi and implement
    Krylov-Schur later on.

--- src/arnoldi/utils.py
+++ src/arnoldi/utils.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+from scipy.linalg import schur
+from scipy.linalg.lapack import ctrexc, dtrexc, strexc, ztrexc
+
 
 def rand_normalized_vector(n, dtype=np.float64):
     """ Create a random normalized vector
@@ -8,3 +11,49 @@ def rand_normalized_vector(n, dtype=np.float64):
     v /= np.linalg.norm(v)
 
     return v
+
+
+_TREXC_FUNCTION = {
+    np.dtype("float32"): strexc,
+    np.dtype("float64"): dtrexc,
+    np.dtype("complex64"): ctrexc,
+    np.dtype("complex128"): ztrexc,
+}
+
+
+def ordered_schur(a, output="real", *, sort_function=None):
+    """Schur decomposition with eigenvalues sorted by the given sorting
+    function.
+    """
+    if output == "complex":
+        complex_dtype = np.result_type(a.dtype, 1j)
+        trexc_function = _TREXC_FUNCTION[complex_dtype]
+    else:
+        trexc_function = _TREXC_FUNCTION[a.dtype]
+
+    if sort_function is None:
+        sort_function = lambda values: np.argsort(np.abs(values))
+
+    T, Z = schur(a, output=output)
+    n = T.shape[0]
+
+    if output == "complex":
+        eigenvalues = np.diag(T)
+        ordered_indices = sort_function(eigenvalues)
+
+        current_pos = list(range(n))
+
+        for target, source_idx in enumerate(ordered_indices):
+            source = current_pos.index(source_idx)
+
+            if source != target:
+                # + 1 is because Fortran/trexc function are 1-based indexing
+                T, Z, info = trexc_function(T, Z, source + 1, target + 1)
+
+                # Update position tracking
+                moved = current_pos.pop(source)
+                current_pos.insert(target, moved)
+    else:
+        raise ValueError("output!='complex' not implemented yet")
+
+    return T, Z
